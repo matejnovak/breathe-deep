e5c20d4d15defed059f6fc788406f10191864939
Matej Novak <matejnovak5@gmail.com>
2016-06-24 00:08:26 +0200

    v4

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-$(function(){ 
+$(function(){
 
   var toggleState = true;
   $('.container').on("click", function (event) {
     if(toggleState) {
-      $(".a, .b, .c, .d, .e").addClass("boom");
-      $(".btn").stop().fadeOut(400);
+      $('.a, .b, .c, .d, .e').addClass("boom");
+      $('.btn').stop().fadeOut(400);
+      $('.notes').animate({opacity: 1}, 3000);
+      $('.tuts span:nth-child(1)').animate({ color: '#222222' }, 500);
+
+      setTimeout(function() {
+        $('.tuts span:nth-child(3)').animate({ color: '#222222' }, 500);
+        $('.tuts span:nth-child(1)').animate({ color: '#167abd' }, 500);
+      }, 4900);
+
+      setTimeout(function() {
+        $('.tuts span:nth-child(5)').animate({ color: '#222222' }, 500);
+        $('.tuts span:nth-child(3)').animate({ color: '#167abd' }, 500);
+      }, 9800);
+
+      setTimeout(function() {
+        $('.tuts span:nth-child(5)').animate({ color: '#167abd' }, 500);
+      }, 14700);
+
     } else {
-      $(".a, .b, .c, .d, .e").removeClass("boom");
-      $(".btn").stop().fadeIn(1000).text("Repeat");
+      $('.a, .b, .c, .d, .e').removeClass('boom');
+      $('.btn').stop().fadeIn(1000).text('Repeat');
     }
     
     setTimeout(function() {
       toggleState = !toggleState;
+      $('.notes').animate({opacity: 0}, 2100);
     }, 500);
+
+    
 
   });
 
