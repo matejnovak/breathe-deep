4656c227d6b470068fc6bdb709e32b6e53a58d46
Matej Novak <matejnovak5@gmail.com>
2016-06-26 12:27:35 +0200

    v5

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 $(function(){
 
   var toggleState = true;
-  $('.container').on("click", function (event) {
-    if(toggleState) {
-      $('.a, .b, .c, .d, .e').addClass("boom");
-      $('.btn').stop().fadeOut(400);
-      $('.notes').animate({opacity: 1}, 3000);
+
+  function animateTuts() {
       $('.tuts span:nth-child(1)').animate({ color: '#222222' }, 500);
 
-      setTimeout(function() {
+      timerOne = setTimeout(function() {
         $('.tuts span:nth-child(3)').animate({ color: '#222222' }, 500);
         $('.tuts span:nth-child(1)').animate({ color: '#167abd' }, 500);
       }, 4900);
 
-      setTimeout(function() {
+      timerTwo = setTimeout(function() {
         $('.tuts span:nth-child(5)').animate({ color: '#222222' }, 500);
         $('.tuts span:nth-child(3)').animate({ color: '#167abd' }, 500);
       }, 9800);
 
-      setTimeout(function() {
+      timerThree = setTimeout(function() {
         $('.tuts span:nth-child(5)').animate({ color: '#167abd' }, 500);
       }, 14700);
+  }
 
+
+  $('.container').on("click", function() {
+    $('.notes').stop();
+    $('.btn').stop();
+    $('.tuts span').stop();
+
+    if(toggleState) {
+      $('.a, .b, .c, .d, .e').addClass("boom");
+      $('.btn').fadeOut(400);
+      $('.notes').animate({opacity: 0}, 3000);
+      animateTuts();
     } else {
       $('.a, .b, .c, .d, .e').removeClass('boom');
-      $('.btn').stop().fadeIn(1000).text('Repeat');
+      $('.btn').fadeIn(1000).text('Repeat');
+      $('.tuts span').animate({ color: '#167abd' }, 500);
+      clearTimeout(timerOne);
+      clearTimeout(timerTwo);
+      clearTimeout(timerThree);
+      $('.notes').text('Breathe, You Are Alive!').animate({opacity: 1}, 4000);
     }
     
-    setTimeout(function() {
-      toggleState = !toggleState;
-      $('.notes').animate({opacity: 0}, 2100);
-    }, 500);
-
-    
+    toggleState = !toggleState;
 
   });
 
